c9180304361ffc66e320e2c16b17a470b911bfe8
yehor1959 <zaitsev.yehor@gmail.com>
2025-02-20 08:57:19 +0100

    Add Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/org/example/tasks/Third.java
@@ -0,0 +1,21 @@
+package org.example.tasks;
+
+public class Third {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) {
+            return root;
+        }
+        if (l == null) {
+            return r;
+        }
+        return l;
+    }
+}
---
+++ src/main/java/org/example/tasks/TreeNode.java
@@ -0,0 +1,8 @@
+package org.example.tasks;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
